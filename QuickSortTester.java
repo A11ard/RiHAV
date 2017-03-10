4f35f599589f9260614c4c9b4462a1b12921613d
A11ard <apeng@stuy.edu>
2017-03-09 23:50:10 -0500

    Update QuickSortTester.java

--- QuickSortTester.java
+++ QuickSortTester.java
@@ -1,66 +1,47 @@
 /** QuickSortTester.
- *
- * ALGORITHM:
- *
- * BIG-OH CLASSIFICATION OF ALGORITHM:
- * 
- *
- * Mean execution times for dataset of size n:
- *  
- *
- * ANALYSIS:
- 
-Length            Time (nanoseconds)
-1245               63051
-allard.peng@cslab3-25:~/Desktop/myHWs/lab01$ java QuickSortTester 
-Running 10000 rounds of testing.
 
-Length            Time (nanoseconds)
-1264               62882
-allard.peng@cslab3-25:~/Desktop/myHWs/lab01$ java QuickSortTester 
-Running 10000 rounds of testing.
+Average: Run the timer on a random array of random length. 
 
-Length            Time (nanoseconds)
-1292               63811
-allard.peng@cslab3-25:~/Desktop/myHWs/lab01$ javac QuickSortTester.java 
-allard.peng@cslab3-25:~/Desktop/myHWs/lab01$ java QuickSortTester 
-Running 10000 rounds of testing.
+Best: Run the timer many times, take the fastest, as this probably approaches a "best case"
 
-Length            Time (nanoseconds)
-2058              105920
-allard.peng@cslab3-25:~/Desktop/myHWs/lab01$ java QuickSortTester 
-Running 10000 rounds of testing.
-
-Length            Time (nanoseconds)
-2044              105788
-allard.peng@cslab3-25:~/Desktop/myHWs/lab01$ javac QuickSortTester.java 
-allard.peng@cslab3-25:~/Desktop/myHWs/lab01$ java QuickSortTester 
-Running 10000 rounds of testing.
-
-Length            Time (nanoseconds)
-3252              170645
-allard.peng@cslab3-25:~/Desktop/myHWs/lab01$ java QuickSortTester 
-Running 10000 rounds of testing.
-
-Length            Time (nanoseconds)
-3432              181040
-allard.peng@cslab3-25:~/Desktop/myHWs/lab01$ java QuickSortTester 
-Running 10000 rounds of testing.
-
-Length            Time (nanoseconds)
-3311              176324
+Worst: Presort the array. Then run the timer. 
 
  */
 
 public class QuickSortTester {
     private static final long ROUNDS = 10000;
-    private static final int    SIZE = 3000 + (int)( Math.random() * 1000 );
+    private static final int    SIZE = 1000 + (int)( Math.random() * 1000 );
 
     /** Execution time analysis.
      * Create an array of size SIZE and populate it with arbitary values and
      * run merge sort on the array. Repeat ROUNDS times, then divide the total
-     * amount of time spent by ROUNDS to obtain the average.
+     * amount of time spent by ROUNDS to obtain the average
      */
+    
+    public static void swap( int x, int y, int[] o ) {
+	int tmp = o[x];
+	o[x] = o[y];
+	o[y] = tmp;
+    }
+    public static void shuffle( int[] d ) {
+	int tmp;
+	int swapPos;
+	for( int i = 0; i < d.length; i++ ) {
+	    tmp = d[i];
+	    swapPos = i + (int)( (d.length - i) * Math.random() );
+	    swap( i, swapPos, d );
+	}
+    }
+    public static void printArr( long[] a ) {
+	for ( long o : a )
+	    System.out.print( o + " " );
+	System.out.println();
+    }
+    public static int factorial(int n){
+	if(n == 1) return 1;
+	else return (n * factorial(n-1)); 
+    }
+    
     public static void main( String[] args ) {
         System.out.println("Running " + ROUNDS + " rounds of testing.\n");
         System.out.format("%s%30s%n", "Length", "Time (nanoseconds)");
@@ -68,94 +49,48 @@ public class QuickSortTester {
         long time = 0;
         long s = 0;
         long e = 0;
-        int[] data1 = new int[1];
-	int[] data2 = new int[10];
-	int[] data3 = new int[100];
-	int[] data4 = new int[1000];
-	int[] data5 = new int[10000];
-	int[] data6 = new int[100000];
+        int[] data = new int[SIZE];
 
-	//warm-up
-	for ( long rounds = ROUNDS; rounds > 0; rounds-- ) {
-            for ( int i = 0; i < data2.length; i++ ) {
-                data2[i] = (int) ( 1000 * Math.random() );
-            }
-            
-            QuickSort.qsort(data2);
-	                               
-        }
-	
-        
-	//size 1 array
-	time = 0;
+	//this is for testing best case
+	int[] a = new int[8]; 
+	long[] stored = { 987654321, 987654321, 987654321 };
+	long TIME = 0; 
+
         for ( long rounds = ROUNDS; rounds > 0; rounds-- ) {
-            for ( int i = 0; i < data1.length; i++ ) {
-                data1[i] = (int) ( 1000 * Math.random() );
+            for ( int i = 0; i < data.length; i++ ) {
+                data[i] = (int) ( 1000 * Math.random() );
             }
+	    
+            //if you want to make the array the worst case (first sort the array) UNCOMMENT THE NEXT LINE 
+	    QuickSort.qsort(data);//sort the array before running the qsort algo
+	    
             s = System.nanoTime();
-            QuickSort.qsort(data1);
+            QuickSort.qsort(data);
             e = System.nanoTime();
 
             time += (e - s);
         }
-        System.out.format("%s%20s%n", data1.length, time / ROUNDS);
 
-	//size 10 array
-	time = 0;
-	for ( long rounds = ROUNDS; rounds > 0; rounds-- ) {
-            for ( int i = 0; i < data2.length; i++ ) {
-                data2[i] = (int) ( 1000 * Math.random() );
-            }
+ 	//for best case, there doesnt seem to be a way to just construct a best case array. Therefore, I'll use an array of length n and test it n! * 100 times. Since an array of size n has n! combinations for elements, and only 1 or 2 configurations are best case, I'll run it n! * 100 times, and take the fastest 1%, assuming that those are closer to best case
+	/*       for(int i = 0; i < (factorial(a.length) ); i++){
+ 
+	    shuffle(a);
+	    
             s = System.nanoTime();
-            QuickSort.qsort(data2);
+            QuickSort.qsort(data);
             e = System.nanoTime();
 
-            time += (e - s);
-        }
-        System.out.format("%s%20s%n", data2.length, time / ROUNDS);
+            TIME = (e - s);
 
-	//size 100 array
-	time = 0;
-	for ( long rounds = ROUNDS; rounds > 0; rounds-- ) {
-            for ( int i = 0; i < data3.length; i++ ) {
-                data3[i] = (int) ( 1000 * Math.random() );
-            }
-            s = System.nanoTime();
-            QuickSort.qsort(data3);
-            e = System.nanoTime();
-
-            time += (e - s);
-        }
-        System.out.format("%s%20s%n", data3.length, time / ROUNDS);
-
-	//size 1000 array
-	time = 0;
-	for ( long rounds = ROUNDS; rounds > 0; rounds-- ) {
-            for ( int i = 0; i < data4.length; i++ ) {
-                data4[i] = (int) ( 1000 * Math.random() );
-            }
-            s = System.nanoTime();
-            QuickSort.qsort(data4);
-            e = System.nanoTime();
-
-            time += (e - s);
-        }
-        System.out.format("%s%20s%n", data4.length, time / ROUNDS);
-
-	//size 10000 array
-	time = 0;
-	for ( long rounds = ROUNDS; rounds > 0; rounds-- ) {
-            for ( int i = 0; i < data5.length; i++ ) {
-                data5[i] = (int) ( 1000 * Math.random() );
-            }
-            s = System.nanoTime();
-            QuickSort.qsort(data5);
-            e = System.nanoTime();
-
-            time += (e - s);
-        }
-        System.out.format("%s%20s%n", data5.length, time / ROUNDS);
-
+	    if(TIME < stored[0]) stored[0] = TIME;
+	    else if(TIME < stored[1]) stored[1] = TIME;
+	    else if(TIME < stored[2]) stored[2] = TIME;
+	    else ; 
+	}
+	System.out.println(a.length + "  |  ");
+	printArr(stored);*/ 	
+	//=========================================================END OF THE BEST CASE TEST 
 	
+        System.out.format("%s%20s%n", data.length, time / ROUNDS);
     }
 }
