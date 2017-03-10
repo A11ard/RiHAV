82781a9f05439e2e06c4c0bd2c1cdc8bee89aecf
A11ard <apeng@stuy.edu>
2017-03-09 23:51:54 -0500

    Update QuickSort.java

--- QuickSort.java
+++ QuickSort.java
@@ -1,7 +1,3 @@
-//Victor Teoh
-//APCS2 pd3
-//HW15 -- HW #15: So So Quick
-//2017--3--09
 /*****************************************************
  * class QuickSort
  * 
@@ -73,11 +69,10 @@ public class QuickSort
     }
     public static void qsorth(int[] arr, int left, int right){
 	if(left < right){
-	    int pvtpos = partition (arr, left, right, (left + right)/2);
+	    int pvtpos = partition (arr, left, right, left+1);
 	    qsorth(arr,left,pvtpos-1);
 	    qsorth(arr,pvtpos + 1, right);
 	}
-
     }
 
     // Thinkers are encouraged to roll their own subroutines.
@@ -99,67 +94,5 @@ public class QuickSort
 	
 	return s;
     }
-    //main method for testing
-    public static void main( String[] args ) 
-    {
-
-	//get-it-up-and-running, static test case:
-	int [] arr1 = {7,1,5,12,3};
-	System.out.println("\narr1 init'd to: " );
-	printArr(arr1);
-
-	qsort( arr1 );	
-       	System.out.println("arr1 after qsort: " );
-	printArr(arr1);
-
-
-	// randomly-generated arrays of n distinct vals
-	int[] arrN = new int[10];
-	for( int i = 0; i < arrN.length; i++ )
-	    arrN[i] = i;
-       
-	System.out.println("\narrN init'd to: " );
-	printArr(arrN);
-
-       	shuffle(arrN);
-       	System.out.println("arrN post-shuffle: " );
-	printArr(arrN);
-
-	qsort( arrN );
-	System.out.println("arrN after sort: " );
-	printArr(arrN);
-
-
-
-
-
-	//get-it-up-and-running, static test case w/ dupes:
-	int [] arr2 = {7,1,5,12,3,7};
-	System.out.println("\narr2 init'd to: " );
-	printArr(arr2);
-
-	qsort( arr2 );	
-       	System.out.println("arr2 after qsort: " );
-	printArr(arr2);
-
-
-	// arrays of randomly generated ints
-	int[] arrMatey = new int[20];
-	for( int i = 0; i < arrMatey.length; i++ )
-	    arrMatey[i] = (int)( 48 * Math.random() );
-       
-	System.out.println("\narrMatey init'd to: " );
-	printArr(arrMatey);
-
-       	shuffle(arrMatey);
-       	System.out.println("arrMatey post-shuffle: " );
-	printArr(arrMatey);
-
-	qsort( arrMatey );
-	System.out.println("arrMatey after sort: " );
-	printArr(arrMatey);
-
-
-    }//end main
 
 }//end class QuickSort
